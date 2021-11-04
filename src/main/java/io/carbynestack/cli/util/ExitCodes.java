9c616d9000fedc6cc7575d9d92f56f34da8ee2b8
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-11-04 13:21:23 +0100

    Add ExitCodes.success (#47)
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>
    Signed-off-by: Sebastian Becker <Sebastian.Becker@de.bosch.com>

---
+++ src/main/java/io/carbynestack/cli/util/ExitCodes.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/carbynestack/cli-ng.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package io.carbynestack.cli.util;
+
+import io.carbynestack.common.CsFailureReason;
+import io.carbynestack.common.result.Result;
+import io.carbynestack.common.result.Success;
+
+/**
+ * Represents a collection of exit codes in {@link Result} form.
+ *
+ * @see Result
+ * @since 0.2.0
+ */
+public interface ExitCodes extends CsFailureReason {
+    /**
+     * Returns the success exit code {@code 0} as a {@link Result}.
+     *
+     * @param <F> the failure reason type
+     * @return a {@code Success} with value 0
+     * @see Result
+     * @since 0.2.0
+     */
+    static <F extends CsFailureReason> Result<Integer, F> success() {
+        return new Success<>(0);
+    }
+}
