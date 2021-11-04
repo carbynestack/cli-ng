a79b9a54f5e97557f1ce83d1774de1f9008a7591
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-11-04 13:12:12 +0100

    Add missing test
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

---
+++ src/test/java/io/carbynestack/cli/util/args/NoArgTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/carbynestack/cli-ng.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package io.carbynestack.cli.util.args;
+
+import org.junit.jupiter.api.Test;
+
+public class NoArgTest {
+    @Test
+    public void constructor() {
+        new NoArg();
+    }
+}
