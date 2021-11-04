ac163eba8e64fb53d509c17432aef83c2f4983ae
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-11-04 12:34:05 +0100

    Add Verbosity enum
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

---
+++ src/test/java/io/carbynestack/cli/util/VerbosityTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/carbynestack/cli-ng.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package io.carbynestack.cli.util;
+
+import org.junit.jupiter.api.Test;
+
+import static io.carbynestack.cli.util.Verbosity.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VerbosityTest {
+    @Test
+    public void fromReturnsDefault() {
+        assertThat(Verbosity.from(new boolean[0])).isEqualTo(DEFAULT);
+    }
+
+    @Test
+    public void fromReturnsVerbose() {
+        assertThat(Verbosity.from(new boolean[]{true})).isEqualTo(VERBOSE);
+    }
+
+    @Test
+    public void fromReturnsExtraVerbose() {
+        assertThat(Verbosity.from(new boolean[]{true, true})).isEqualTo(EXTRA_VERBOSE);
+    }
+
+    @Test
+    public void fromReturnsDebug() {
+        assertThat(Verbosity.from(new boolean[]{true, true, true})).isEqualTo(DEBUG);
+    }
+
+    @Test
+    public void fromOverflow() {
+        assertThat(Verbosity.from(new boolean[]{true, true, true, true})).isEqualTo(DEFAULT);
+    }
+}
