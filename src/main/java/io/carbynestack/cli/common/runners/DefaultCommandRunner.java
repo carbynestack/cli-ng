b0ef0ef53fb0890a069973bd849f2aa05360b6e7
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-11-08 12:40:21 +0100

    Add missing DefaultCommandRunner documentation
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

--- src/main/java/io/carbynestack/cli/common/runners/DefaultCommandRunner.java
+++ src/main/java/io/carbynestack/cli/common/runners/DefaultCommandRunner.java
@@ -18,15 +18,40 @@ import static io.carbynestack.cli.common.CommandExecutor.execute;
 import static java.util.Objects.requireNonNull;
 import static picocli.CommandLine.Mixin;
 
+/**
+ * The default {@link CommandRunner} implementation.
+ *
+ * @since 0.4.0
+ */
 public class DefaultCommandRunner implements CommandRunner<NoArg>, Callable<Integer> {
+    /**
+     * The common command options shared by all runners implementing {@link CommandRunner}.
+     *
+     * @since 0.4.0
+     */
     @Mixin
     protected Common common;
 
+    /**
+     * Default implementation of the runner logic which uses the {@link HelpRunner}
+     * if not overridden.
+     *
+     * @param noArgs the ignored command arguments
+     * @param common the common command options
+     * @return the {@code HelpRunner} exit code
+     * @since 0.4.0
+     */
     @Override
     public Result<Integer, ? extends CsFailureReason> run(NoArg noArgs, Common common) {
         return requireNonNull(new HelpRunner().run(noArgs, common));
     }
 
+    /**
+     * Integration of the CLI execution model with picocli's resolving.
+     *
+     * @return the exit code
+     * @since 0.4.0
+     */
     @Override
     public Integer call() {
         return execute(() -> this, new NoArg(), common);
