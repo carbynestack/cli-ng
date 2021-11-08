a4dfd01b80febe5b511b519929bad3fda933bbdc
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-11-08 12:46:28 +0100

    Add missing CommandExecutionStrategy documentation
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

--- src/main/java/io/carbynestack/cli/common/CommandExecutionStrategy.java
+++ src/main/java/io/carbynestack/cli/common/CommandExecutionStrategy.java
@@ -14,7 +14,28 @@ import picocli.CommandLine.*;
 
 import static java.util.function.Function.identity;
 
+/**
+ * CLI specific implementation responsible for "executing"
+ * the user input and returning an exit code.
+ *
+ * <p>The {@code CommandExecutionStrategy} handles the
+ * help or version requests or otherwise delegates the
+ * invokations to the {@link RunLast} strategy.
+ *
+ * @since 0.4.0
+ */
 public class CommandExecutionStrategy implements IExecutionStrategy {
+    /**
+     * {@inheritDoc}
+     *
+     * @param parseResult the parse result from which to select one or more {@code CommandSpec}
+     *                    instances to execute.
+     * @return an exit code
+     * @throws ParameterException if the invoked method on the CommandSpec's user object threw
+     *                            a ParameterException to signify invalid user input.
+     * @throws ExecutionException if any problem occurred while executing the command.
+     * @since 0.4.0
+     */
     @Override
     public int execute(ParseResult parseResult) throws ExecutionException, ParameterException {
         record IsHelp<T>(boolean is, T other) {
