ceb44cbc66a55a7692ad471b004ad4d751611680
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-11-15 15:15:28 +0100

    Add output format enum
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

---
+++ src/main/java/io/carbynestack/cli/util/Format.java
@@ -0,0 +1,27 @@
+package io.carbynestack.cli.util;
+
+/**
+ * The CLI output format.
+ *
+ * @since 0.5.0
+ */
+public enum Format {
+    /**
+     * Strip all formatting
+     *
+     * @since 0.5.0
+     */
+    PLAIN,
+    /**
+     * ANSI formatted text
+     *
+     * @since 0.5.0
+     */
+    DEFAULT,
+    /**
+     * JSON structure
+     *
+     * @since 0.5.0
+     */
+    JSON;
+}
