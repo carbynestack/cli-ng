35de511bb2ef3ba7f41628ee14596733214ab432
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-12-17 09:36:16 +0100

    Add required option marker in usage/help
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

--- src/main/java/io/carbynestack/cli/CsCLI.java
+++ src/main/java/io/carbynestack/cli/CsCLI.java
@@ -21,7 +21,7 @@ import static picocli.CommandLine.ScopeType.INHERIT;
  */
 @Command(name = "cs", description = "Command Line Interface to interact with Carbyne Stack Virtual Clouds",
         scope = INHERIT, usageHelpAutoWidth = true, showEndOfOptionsDelimiterInUsageHelp = true,
-        mixinStandardHelpOptions = true, showAtFileInUsageHelp = true,
+        mixinStandardHelpOptions = true, showAtFileInUsageHelp = true, requiredOptionMarker = '*',
         subcommands = {CompletionCommand.class})
 public class CsCLI extends DefaultCommandRunner {
     /**
