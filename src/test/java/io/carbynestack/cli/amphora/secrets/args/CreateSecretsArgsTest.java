d38d3fcc44ca6fa8ec25ec89a499a5862b184b75
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-12-17 14:44:11 +0100

    Add CreateSecretArgs tests
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

---
+++ src/test/java/io/carbynestack/cli/amphora/secrets/args/CreateSecretsArgsTest.java
@@ -0,0 +1,40 @@
+package io.carbynestack.cli.amphora.secrets.args;
+
+import io.carbynestack.testing.nullable.NullableParamSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CreateSecretsArgsTest {
+    private static final UUID ID = UUID.randomUUID();
+    private static final Map<String, String> RAW_TAGS = Map.of(
+            "first", "1", "second", "2"
+    );
+    private static final BigInteger[] SECRETS = new BigInteger[]{
+            BigInteger.ONE, BigInteger.TEN
+    };
+
+    @SuppressWarnings("unused")
+    private static final Arguments PARAMS = Arguments.of(ID, RAW_TAGS, SECRETS);
+
+    @Test
+    void constructor() {
+        var args = new CreateSecretsArgs(ID, RAW_TAGS, SECRETS);
+        assertThat(args.id()).isEqualTo(ID);
+        assertThat(args.rawTags()).isEqualTo(RAW_TAGS);
+        assertThat(args.secrets()).isEqualTo(SECRETS);
+    }
+
+    @ParameterizedTest
+    @NullableParamSource("PARAMS")
+    void constructorNullableValues(UUID id, Map<String, String> rawTags, BigInteger[] secrets) {
+        assertThatThrownBy(() -> new CreateSecretsArgs(id, rawTags, secrets));
+    }
+}
