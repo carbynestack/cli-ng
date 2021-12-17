a2cf8a858a217b0132e6afe9b7e4957009bd5afe
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-12-17 09:53:23 +0100

    Add missing headers
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

--- src/main/java/io/carbynestack/cli/ephemeral/EphemeralFailureReasons.java
+++ src/main/java/io/carbynestack/cli/ephemeral/EphemeralFailureReasons.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/carbynestack/cli-ng.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package io.carbynestack.cli.ephemeral;
 
 import io.carbynestack.common.CsFailureReason;
