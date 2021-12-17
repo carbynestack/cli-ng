3840d3e31c408f652eeb09d35ef28c7843f1d9cc
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-12-17 13:42:39 +0100

    Add missing headers
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

--- src/test/java/io/carbynestack/cli/castor/TelemetryCommandTest.java
+++ src/test/java/io/carbynestack/cli/castor/TelemetryCommandTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/carbynestack/cli-ng.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package io.carbynestack.cli.castor;
 
 import io.carbynestack.castor.client.download.CastorIntraVcpClient;
